06f4f19f43c746083555bd1c067cae177b4296ff
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2021-11-18 22:08:57 -0500

    completed upto my_none? 6 of 10

--- custom_enum.rb
+++ custom_enum.rb
@@ -24,6 +24,47 @@ module Enumerable
         end
     end
 
+    def my_select
+        if block_given?
+            result = self.is_a?(Hash) ? {} : []
+            for element in self
+                if yield element
+                    self.is_a?(Hash) ? result.store(element[0], element[1]) : result << element
+                end
+            end
+        else
+            puts "Appropriate block not provided"
+        end
+        result    
+    end
+
+    def my_all?
+        if block_given?
+            self.my_each {|element| return false unless yield element}
+            true
+        else
+            puts "Appropriate block not provided"
+        end
+    end
+
+    def my_any?
+        if block_given?
+            self.my_each {|element| return true if yield element}
+            false
+        else
+            puts "Appropriate block not provided"
+        end
+    end
+
+    def my_none?
+        if block_given?
+            self.my_each {|element| return false if yield element}
+            true
+        else
+            puts "Appropriate block not provided"
+        end
+    end
+
 end
 
 
--- tests.rb
+++ tests.rb
@@ -2,7 +2,7 @@
 require './custom_enum.rb'
 
 include Enumerable
-numbers = [1,2,3,4,5]
+numbers = [1,2,3,4,5,]
 
 
 puts ".each vs my_each"
@@ -16,3 +16,27 @@ puts ".each_with_index"
 numbers.each_with_index {|val, index| puts "value:#{val} index:#{index}"}
 puts ".my_each_with_index"
 numbers.my_each_with_index {|val, index| puts "value:#{val} index:#{index}"}
+
+puts "select vs my_select"
+puts "select"
+puts numbers.select {|num| num.even?}
+puts "my_select"
+puts numbers.my_select {|num| num.even?}
+
+puts "all? vs my_all?"
+puts "all?"
+puts numbers.all? {|num| num.is_a?(Integer)}
+puts "my_all?"
+puts numbers.my_all? {|num| num.is_a?(Integer)}
+
+puts "any? vs my_any?"
+puts "any?"
+puts numbers.any? {|num| num > 4}
+puts "my_any?"
+puts numbers.my_any? {|num| num > 4}
+
+puts "none? vs my_none?"
+puts "none?"
+puts numbers.none? {|num| num == 6}
+puts "my_none?"
+puts numbers.my_none? {|num| num == 6}
